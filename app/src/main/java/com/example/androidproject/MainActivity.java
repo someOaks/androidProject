9df365fa135bdee593fa0856bfa180616472c849
Glebuar <glebuar@gmail.com>
2019-05-13 04:59:40 +0300

    SecondActivity

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnFirstAct);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity();
+            }
+        });
+    }
+
+    private void moveToSecondActivity() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class );
+        startActivity(intent);
     }
 }
